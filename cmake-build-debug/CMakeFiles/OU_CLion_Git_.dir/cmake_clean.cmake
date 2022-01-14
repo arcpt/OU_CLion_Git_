347e89f41e3b651b4ff60276f2e47556eb3fa6d1
sebas <sebastiangac123@gmail.com>
2022-01-14 22:13:37 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_CLion_Git_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_CLion_Git_.dir/main.cpp.obj"
+  "OU_CLion_Git_.exe"
+  "OU_CLion_Git_.exe.manifest"
+  "OU_CLion_Git_.pdb"
+  "libOU_CLion_Git_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_CLion_Git_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
